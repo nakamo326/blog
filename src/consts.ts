6327ff73a77d748ea55e2bc2bdf7fa457c08b355
Yasufumi Nakamoto <nakamo326@gmail.com>
2024-01-05 15:21:27 +0900

    fix site name

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "nakamo326's log";
+export const SITE_DESCRIPTION = "nakamo326のいろいろを置く場所";
