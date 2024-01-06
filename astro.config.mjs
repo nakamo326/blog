db425b1f544c8806e75bfe2649c1e0b98119e966
Yasufumi Nakamoto <nakamo326@gmail.com>
2024-01-06 23:03:04 +0900

    format

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-import icon from "astro-icon";
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import icon from 'astro-icon';
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://nakamo326.com",
-  integrations: [mdx(), sitemap(), icon(), tailwind()]
+  site: 'https://nakamo326.com',
+  integrations: [mdx(), sitemap(), icon(), tailwind()],
 });
