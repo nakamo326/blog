ac72af5fb1193a7e8013b643d9f795cf9fb59a14
Yasufumi Nakamoto <nakamo326@gmail.com>
2024-01-06 21:56:30 +0900

    add tailwind plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://nakamo326.com",
-  integrations: [mdx(), sitemap(), icon()],
+  integrations: [mdx(), sitemap(), icon(), tailwind()]
 });
